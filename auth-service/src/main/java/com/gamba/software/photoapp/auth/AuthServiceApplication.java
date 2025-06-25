49d9939bda0f440edf359123b89a8d955e007429
Marlon Gamba <marlonpgamba@gmail.com>
2025-06-24 23:19:08 -0300

    Removing unused imports

--- auth-service/src/main/java/com/gamba/software/photoapp/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/gamba/software/photoapp/auth/AuthServiceApplication.java
@@ -1,6 +1,8 @@
 package com.gamba.software.photoapp.auth;
 
+import com.gamba.software.photoapp.shared.jwt.JwtService;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
