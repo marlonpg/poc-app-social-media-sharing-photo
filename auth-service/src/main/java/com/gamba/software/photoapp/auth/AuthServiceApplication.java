a50fef32138b227a1206c95564e7bc6457d4a890
Marlon Gamba <marlonpgamba@gmail.com>
2025-06-27 20:50:04 -0300

    Fixing issues cause by Jules.

--- auth-service/src/main/java/com/gamba/software/photoapp/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/gamba/software/photoapp/auth/AuthServiceApplication.java
@@ -1,8 +1,6 @@
 package com.gamba.software.photoapp.auth;
 
-import com.gamba.software.photoapp.shared.jwt.JwtService;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
